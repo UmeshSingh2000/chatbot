7fbb7def4d4a641de237383a7d256c1bf410f720
Umesh singh <umeshsinghmehta4@gmail.com>
2025-08-15 14:59:24 +0530

    feat: add ScrollArea component and integrate it into the chat page layout

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,21 +1,49 @@
 'use client'
+
 import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 import { getCookie } from 'cookies-next'
-const page = () => {
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const Page = () => {
     const router = useRouter()
 
     useEffect(() => {
         const token = getCookie('authToken')
-        if(!token){
+        if (!token) {
             router.push('/')
         }
     }, [router])
+
     return (
-        <div>
-            chat here
+        <div className="grid h-screen grid-cols-[260px_1fr] text-white bg-[#181818]">
+            {/* Sidebar */}
+            <aside className="bg-[#181818] p-4 flex flex-col">
+                <h2 className="text-xl font-bold mb-6">My Chat</h2>
+
+            </aside>
+
+            {/* Main Chat Area */}
+            <main className="flex flex-col bg-[#212121]">
+                {/* Chat Messages */}
+                <div className="flex-1 overflow-y-auto p-6 space-y-4">
+                    <div className='flex justify-center items-center h-full'>
+                        <h1 className='text-4xl'>What can I help with?</h1>
+                    </div>
+                </div>
+
+                {/* Input Bar */}
+                <footer className="bg-[#303030] p-3 flex space-x-3">
+                    <Input
+                        className="flex-1 bg-[#1e1e1e] border-none text-white focus-visible:ring-0 focus-visible:ring-offset-0"
+                        placeholder="Type your message..."
+                    />
+                    <Button className=" cursor-pointer">Send</Button>
+                </footer>
+            </main>
         </div>
     )
 }
 
-export default page
+export default Page
